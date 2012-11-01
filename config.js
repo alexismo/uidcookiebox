e178b2224344af778fd97298d13b3c6a93372917
Alexis Morin <carignan.boy@gmail.com>
2012-11-01 22:43:37 +0100

    removed code

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ var config = {};
 
 config.port = 8080;
 config.address = 'localhost';
-config.twitter_username = process.env.TWITTER_USERNAME || 'uidcookiebox';
-config.twitter_password = process.env.TWITTER_PASSWORD || 'arduino';
+config.twitter_username = process.env.TWITTER_USERNAME || '';
+config.twitter_password = process.env.TWITTER_PASSWORD || '';
 
 config.keyword = 'uidcookiebox';
 
